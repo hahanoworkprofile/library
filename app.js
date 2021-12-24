dc675671def4f408da8a648725631a2425aa1462
cloud <terakorn.p2@gmail.com>
2021-12-24 19:17:30 +0700

    add book form, reworked render system fixed duplicatre rendering, error show, done!

--- app.js
+++ app.js
@@ -14,27 +14,47 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-let test = new Book("unknow", "unknows", "231", false);
-addBookToLibrary(test);
-let test2 = new Book("unknow2", "unknows", "235", true);
-addBookToLibrary(test2);
-
+function render() { 
+    while (list.firstChild) list.removeChild(list.firstChild);
+    errorText.textContent = "Errors: 0";
+    myLibrary.forEach(book => {
+        console.log(book);
+        let card = document.createElement('div'); card.classList.add('card');
+        let title = document.createElement('p'); title.textContent = book.title
+        let author = document.createElement('p'); author.textContent = book.author;
+        let pages = document.createElement('p'); pages.textContent = book.pages;
+        let read = document.createElement('checkbox'); read.value = book.read;
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(read);
+    
+        list.appendChild(card);
+    })
+}
 
 const list = document.querySelector('.list');
+const addBookButton = document.querySelector('.addBook');
+const form = document.querySelector('.addForm');
+const confirmButton = document.querySelector('.confirm')
+const errorText = document.querySelector('.error');
+
+addBookButton.addEventListener("click", e =>
+    form.style.display != "none" ? form.style.display = "none" : form.style.display = "block");  
 
 
-myLibrary.forEach(book => {
-    console.log(book);
-    let card = document.createElement('div'); card.classList.add('card');
-    let title = document.createElement('p'); title.textContent = book.title
-    let author = document.createElement('p'); author.textContent = book.author;
-    let pages = document.createElement('p'); pages.textContent = book.pages;
-    let read = document.createElement('p'); read.TextContent = book.read;
-    card.appendChild(title);
-    card.appendChild(author);
-    card.appendChild(pages);
-    card.appendChild(read);
 
-    list.appendChild(card);
-})
-
+confirmButton.addEventListener("click", e => {
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let pages = document.querySelector('#pages').value; 
+    let read = document.querySelector('#read').value; 
+    let duplicate = false; 
+    myLibrary.forEach(book => book.title == title ? duplicate = true : null);
+    if (duplicate) {
+        errorText.textContent = "Duplicate!";
+    } else {
+        addBookToLibrary(new Book(title, author, pages, read)); 
+        render();
+    }
+});
